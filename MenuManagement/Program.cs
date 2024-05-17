cb6e7f2ebc8bedb6690dde8588ad604d312791cc
AlanKhamo <alankhamo@gmail.com>
2024-05-17 15:59:07 +0200

    .

--- MenuManagement/Program.cs
+++ MenuManagement/Program.cs
@@ -68,20 +68,20 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Server v1");
-    c.RoutePrefix = "/grp-13/webmenuapi/StudyHealthAPI/swagger";
-});
+// app.UseSwaggerUI(c =>
+// {
+//     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Server v1");
+//     c.RoutePrefix = "/grp-13/webmenuapi/StudyHealthAPI/swagger";
+// });
 
-app.UsePathBase("/grp-13/webmenuapi");
-// app.UseSwagger();
-// app.UseSwaggerUI();
+// app.UsePathBase("/grp-13/webmenuapi");
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.MapGet("/", async context =>
-{
-    context.Response.Redirect("/grp-13/webmenuapi/swagger/index.html");
-});
+// app.MapGet("/", async context =>
+// {
+//     context.Response.Redirect("/grp-13/webmenuapi/swagger/index.html");
+// });
 
 app.UseHttpsRedirection();
 app.UseCors();
